7d85d886915ce6db68d36364dfe94e2f525d012e
espanderlof <jaime717@hotmail.es>
2024-04-13 17:29:33 -0400

    Creación services

---
+++ src/main/java/com/jzs/ms_registros_usuarios/service/RegUsuService.java
@@ -0,0 +1,13 @@
+package com.jzs.ms_registros_usuarios.service;
+
+import com.jzs.ms_registros_usuarios.model.Usuario;
+import java.util.List;
+import java.util.Optional;
+
+public interface RegUsuService {
+    List<Usuario> getAllUsuarios();
+    Optional<Usuario> getUsuarioById(Long id);
+    Usuario createUsuario(Usuario usuario);
+    Usuario updateUsuario(Long id, Usuario usuario);
+    void deleteUsuario(Long id);
+}
---
+++ src/main/java/com/jzs/ms_registros_usuarios/service/RegUsuServicelmpl.java
@@ -0,0 +1,46 @@
+package com.jzs.ms_registros_usuarios.service;
+
+import com.jzs.ms_registros_usuarios.model.Usuario;
+import com.jzs.ms_registros_usuarios.repository.RegUsuRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RegUsuServicelmpl implements RegUsuService {
+    
+    @Autowired
+    private RegUsuRepository regUsuRepository;
+
+    @Override
+    public List<Usuario> getAllUsuarios() {
+        return regUsuRepository.findAll();
+    }
+
+    @Override
+    public Optional<Usuario> getUsuarioById(Long id) {
+        return regUsuRepository.findById(id);
+    }
+
+    @Override
+    public Usuario createUsuario(Usuario usuario) {
+        return regUsuRepository.save(usuario);
+    }
+
+    @Override
+    public Usuario updateUsuario(Long id, Usuario usuario) {
+        if (regUsuRepository.existsById(id)) {
+            usuario.setId(id);
+            return regUsuRepository.save(usuario);
+        }else{
+            return null;
+        }
+    }
+
+    @Override
+    public void deleteUsuario(Long id) {
+        regUsuRepository.deleteById(id);
+    }
+}
