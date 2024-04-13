c36f0be41a4640ee1a360a9c876500785846a16f
espanderlof <jaime717@hotmail.es>
2024-04-13 17:14:31 -0400

    Creación repository

---
+++ src/main/java/com/jzs/ms_registros_usuarios/repository/RegUsuRepository.java
@@ -0,0 +1,9 @@
+package com.jzs.ms_registros_usuarios.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jzs.ms_registros_usuarios.model.Usuario;
+
+public interface RegUsuRepository extends JpaRepository<Usuario, Long>{
+    
+}
