4ce80efd26d6325e783b350f635b8a8271288112
espanderlof <jaime717@hotmail.es>
2024-04-13 16:43:30 -0400

    Inicio proyecto

---
+++ src/main/java/com/jzs/ms_registros_usuarios/MsRegistrosUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.jzs.ms_registros_usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRegistrosUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRegistrosUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jzs/ms_registros_usuarios/MsRegistrosUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jzs.ms_registros_usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRegistrosUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
