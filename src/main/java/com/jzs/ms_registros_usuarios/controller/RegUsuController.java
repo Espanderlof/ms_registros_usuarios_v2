176c7a8a13ff5693978ff13c9a0f74c2bd69bc28
espanderlof <jaime717@hotmail.es>
2024-04-14 18:11:12 -0400

    quitar import no usado en controller

--- src/main/java/com/jzs/ms_registros_usuarios/controller/RegUsuController.java
+++ src/main/java/com/jzs/ms_registros_usuarios/controller/RegUsuController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
-import com.fasterxml.jackson.databind.exc.InvalidDefinitionException;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.annotation.Validated;
 
